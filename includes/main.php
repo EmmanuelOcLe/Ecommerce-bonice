55ab1aaedab28cd9bdd45d72b1a32e4ca83b2bc5
Eileen Lidueñez <eileenlc0509@gmail.com>
2025-04-12 22:58:53 -0500

    correccion de main

---
+++ includes/main.php
@@ -0,0 +1,10 @@
+<?php
+    $page = $_GET['page'] ?? 'home';
+    $file = "pages/$page.php";
+    if (file_exists($file)) {
+        include $file;
+    } else {
+        echo "<p style='color:red'>La página solicitada no existe.</p>";
+    }
+?>
+    
