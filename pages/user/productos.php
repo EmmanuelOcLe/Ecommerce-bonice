ba7848ac6fefe64b0fe878a99faed0898af8d28e
Kevin M <Kevinx7276@gmail.com>
2025-04-13 21:53:54 -0500

    Creacion parte vista de producto, ejemplo

--- pages/user/productos.php
+++ pages/user/productos.php
@@ -6,26 +6,35 @@
     <div class="cards-container">
 
         <div class="product-card">
-            <img src="uploads/productos/producto1.png" alt="Producto" class="product-image">
-            <h3 class="product-name">Nombre del Producto</h3>
-            <p class="product-description">Descripción breve del producto.</p>
-            <p class="product-price">$99.99</p>
+            <a href="index.php?page=user/detalle&producto=1">
+                <img src="uploads/productos/producto1.png" alt="Producto" class="product-image">
+                <h3 class="product-name">Popetas Surtidas</h3>
+            </a>
+            <p class="product-description">Sabores surtidos irresistibles.</p>
+            <p class="product-price">$3.500</p>
+            <a href="index.php?page=user/detalle&producto=1" class="view-more-button">Ver Más</a>
             <button class="add-to-cart-button">Agregar al Carrito</button>
         </div>
 
         <div class="product-card">
-            <img src="uploads/productos/producto2.png" alt="Producto" class="product-image">
-            <h3 class="product-name">Nombre del Producto</h3>
-            <p class="product-description">Descripción breve del producto.</p>
-            <p class="product-price">$99.99</p>
+            <a href="index.php?page=user/detalle&producto=2">
+                <img src="uploads/productos/producto2.png" alt="Producto" class="product-image">
+                <h3 class="product-name">Popetas Dulce Saladas</h3>
+            </a>
+            <p class="product-description">Dulce y salado, perfecto equilibrio.</p>
+            <p class="product-price">$3.900</p>
+            <a href="index.php?page=user/detalle&producto=2" class="view-more-button">Ver Más</a>
             <button class="add-to-cart-button">Agregar al Carrito</button>
         </div>
 
         <div class="product-card">
-            <img src="uploads/productos/producto3.png" alt="Producto" class="product-image">
-            <h3 class="product-name">Nombre del Producto</h3>
-            <p class="product-description">Descripción breve del producto.</p>
-            <p class="product-price">$99.99</p>
+            <a href="index.php?page=user/detalle&producto=3">
+                <img src="uploads/productos/producto3.png" alt="Producto" class="product-image">
+                <h3 class="product-name">Triángulo Chocolate</h3>
+            </a>
+            <p class="product-description">Helado con sabor intenso a chocolate.</p>
+            <p class="product-price">$2.900</p>
+            <a href="index.php?page=user/detalle&producto=3" class="view-more-button">Ver Más</a>
             <button class="add-to-cart-button">Agregar al Carrito</button>
         </div>
 
