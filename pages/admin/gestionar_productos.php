f84d6df2db1589fdadd077baa37d8560494b7e86
Emmanuel OcLe <emanuellemus119@gmail.com>
2025-04-19 20:55:07 -0500

    fix-admin-front-end

--- pages/admin/gestionar_productos.php
+++ pages/admin/gestionar_productos.php
@@ -10,9 +10,9 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../../assets/css/style.css">
-  <link rel="stylesheet" href="../../assets/css/global.css">
-  <link rel="stylesheet" href="../../assets/css/productos.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/global.css">
+  <link rel="stylesheet" href="assets/css/productos.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <title>Gestionar productos</title>
 </head>
